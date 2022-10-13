5bbd1f9f349643bf85afd89d6de39e8cffc37a2f
amityi90 <amityi90@gmail.com>
2022-10-13 10:31:52 +0300

    small to big

--- mongo-u.py
+++ mongo-u.py
@@ -8,13 +8,13 @@ import urllib.request
 
 client = MongoClient()
 db = client['faster']
-collection = db['small']
+collection = db['big']
 
 
 
 
-file_name = "web_csv_files/small.csv"
-file_url = "http://35.241.167.153/small.csv"
+file_name = "web_csv_files/big.csv"
+file_url = "http://35.241.167.153/big.csv"
 
 urllib.request.urlretrieve(file_url, file_name)
 rows = []
