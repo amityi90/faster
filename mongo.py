2283d3f57fc50e0b097fc057e96621a5d31a822a
amityi90 <amityi90@gmail.com>
2022-10-13 08:39:39 +0300

    feat: faster

---
+++ mongo.py
@@ -0,0 +1,43 @@
+import csv
+from venv import create
+import pandas as pd
+import math
+import datetime
+import os
+import mmap
+from pymongo import MongoClient
+import urllib.request
+
+
+client = MongoClient()
+db = client['faster']
+collection = db['small']
+
+
+
+
+file_name = "csv_files\\big.csv"
+
+rows = []
+with open(file_name, 'r', encoding="utf8") as file:
+    csvreader = csv.reader(file)
+    found_row  = []
+    time_before_running = datetime.datetime.now()
+    for row in csvreader:
+        address = { 
+            'index' : row[0],
+            'localid' : row[1],
+            'table_name' : row[2],
+            'fulladdress' : row[3],
+            'city_id' : row[4],
+        }
+        collection.insert_one(address).inserted_id
+        if '000205600VK56E' in row:
+            found_row = row
+        print(row)
+        print(row, '\n--------------\n')
+        os.system('cls')
+
+time_before_running = datetime.datetime.now()
+print(collection.find_one({"localid": "3281901VK4738A"}))
+print('mongo: ', datetime.datetime.now() - time_before_running)
