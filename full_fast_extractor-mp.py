b40c488052446375fbaa8a736202b9cec3ca1d0f
amityi90 <amityi90@gmail.com>
2022-10-16 21:43:57 +0300

    pushing to shelve and mongo

--- full_fast_extractor-mp.py
+++ full_fast_extractor-mp.py
@@ -100,6 +100,7 @@ class FastExtractor:
         with open(self.csv_file_name, 'r', encoding="utf8") as file:
             csvreader = csv.reader(file)
             number_of_processes = 100
+            print(number_of_processes)
             self.lines = list(csvreader)
             print(len(self.lines))
             index = 1
