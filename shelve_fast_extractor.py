397f3f773bdbc9b01ac58429cc31d60771509037
amityi90 <amityi90@gmail.com>
2022-10-17 11:54:22 +0300

    fix: time writing

--- shelve_fast_extractor.py
+++ shelve_fast_extractor.py
@@ -3,7 +3,6 @@ import csv
 import math
 import datetime
 import os
-from pymongo import MongoClient
 import urllib.request
 import re
 from multiprocessing import Process
@@ -19,15 +18,13 @@ class FastExtractor:
         self.db_file_name = config('SHELVE_DB_FILE_NAME')
         self.csv_file_name = config('CSV_URL')
         self.results_file = open('results.txt', 'a')
-        self.processes = []
-        self.lines = []
-
 
     def download_csv(self):
         urllib.request.urlretrieve(self.csv_url, self.csv_file_name)
         
-    def push_csv_to_shelve(self):
+    def push_csv_to_shelve_db(self):
         with open(self.csv_file_name, 'r', encoding="utf8") as file:
+            not_uploded_rows = []
             csvreader = csv.reader(file)
             d = shelve.open(self.db_file_name, 'c') 
             for i, row in enumerate(csvreader):
@@ -41,10 +38,14 @@ class FastExtractor:
                     'city_id' : row[4],
                 }
                 key = address['localid']
-                d[key] = address
+                try:
+                    d[key] = address
+                except:
+                    not_uploded_rows.append(row)
                 print(row, f'\n\n#{i} -- in shelve\n\n')
                 print('\n--------------\n')
             d.close()
+            print('\nupload not succeed with: \n\n', not_uploded_rows)
 
     def find_address_shelve(self, address):
         d = shelve.open(self.db_file_name, 'r')  
@@ -67,16 +68,11 @@ class FastExtractor:
             localid = input('\nEnter localid or x to exit: \n')
             if localid != 'x':
                 self.find_address_shelve(localid)
+        print(f'\n\n     ----- -----\n\n     cho')
 
 if __name__ == '__main__':
     addres_session = FastExtractor("http://35.241.167.153/big.csv")
-    # addres_session.push_csv_to_shelve()
-    # addres_session.join_processes()
-    # addres_session.push_csv_to_db()
-    # addres_session.join_processes()
-    addres_session.extract_session()
-
-
-
-    print(f'\n\n     ----- -----\n\n     cho')
+    # addres_session.download_csv()     # download csv file from csv_url
+    # addres_session.push_csv_to_shelve_db()   # creating db files  
+    addres_session.extract_session()    # adderesses search by local id session
 
